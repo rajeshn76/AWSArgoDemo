0e01c6915f74ea3188d1ad4cf4f1e5bd317ed328
Rajesh Narasimhan <rajeshn76@gmail.com>
2018-03-16 17:54:55 -0400

    argo command

--- argo-demo/src/test/java/net/mls/argo/ShellCommandExecutorTest.java
+++ argo-demo/src/test/java/net/mls/argo/ShellCommandExecutorTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.*;
 
 public class ShellCommandExecutorTest {
     @Test
+    public void executeArgoCommand() throws Exception {
+        String output = ShellCommandExecutor.execute("argo version");
+        System.out.println(output);
+    }
+
+    @Test
     public void executeKubernetesCommand() throws Exception {
         String output = ShellCommandExecutor.execute("kubectl version");
         System.out.println(output);
