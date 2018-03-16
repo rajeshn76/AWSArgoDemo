751dc677bd169c206ccb86713a3151a4614f3749
Rajesh Narasimhan <rajeshn76@gmail.com>
2018-03-16 17:29:18 -0400

    fix unit test to run on both windows and linux

--- argo-demo/src/test/java/net/mls/argo/ShellCommandExecutorTest.java
+++ argo-demo/src/test/java/net/mls/argo/ShellCommandExecutorTest.java
@@ -6,12 +6,9 @@ import static org.junit.Assert.*;
 
 public class ShellCommandExecutorTest {
     @Test
-    public void executePingCommand() throws Exception {
-        String output = ShellCommandExecutor.execute("ping google.com");
+    public void executeKubernetesCommand() throws Exception {
+        String output = ShellCommandExecutor.execute("kubectl version");
         System.out.println(output);
-        assertTrue(output.contains("google.com"));
-        assertTrue(output.toLowerCase().contains("ttl="));
-        assertTrue(output.toLowerCase().contains("packets"));
-        assertTrue(output.toLowerCase().contains("received"));
+        assertTrue(output.contains("Client Version:"));
     }
 }
